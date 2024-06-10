bafd15c1245d55d3ce0d1e5932175df4a327b75d
lauraamelina <lauraamelina@gmail.com>
2024-06-10 13:47:13 -0300

    feat: add logic authentication to NavBar.jsx

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,19 +1,42 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus, faHouse, faListUl, faUser, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faHouse, faListUl, faUser, faRightFromBracket, faUserPlus, } from '@fortawesome/free-solid-svg-icons';
 import './NavBar.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { getUser, logout } from '../../services/auth/auth.service';
+import Swal from 'sweetalert2';
 
 export default function NavBar() {
     const [activeIndex, setActiveIndex] = useState(0);
+    const user = getUser();
+    const navigate = useNavigate()
 
-    const menuItems = [
+    const handleLogout = () => {
+        logout();
+        Swal.fire({
+            title: "Sesión cerrada correctamente",
+            text: "Tu sesión se ha cerrado correctamente. ¡Hasta pronto!",
+            icon: "success",
+            confirmButtonColor: "#0f3460",
+            willClose: () => {
+                navigate('/login');
+            }
+        });
+    };
+
+    const menuItems = user ? [
         { icon: faHouse, tooltip: 'Inicio', to: '/' },
         { icon: faPlus, tooltip: 'Agregar Tarea', to: '/add' },
         { icon: faListUl, tooltip: 'Lista de Tareas', to: '/list' },
         { icon: faUser, tooltip: 'Perfil', to: '/perfil' },
-        { icon: faRightFromBracket, tooltip: 'Cerrar Sesión', to: '/' },
+        { icon: faRightFromBracket, tooltip: 'Cerrar Sesión', onClick: handleLogout },
+    ] : [
+        { icon: faHouse, tooltip: 'Inicio', to: '/' },
+        { icon: faUser, tooltip: 'Iniciar Sesión', to: '/login' },
+        { icon: faUserPlus, tooltip: 'Registrarse', to: '/registro' },
     ];
+
+
 
     return (
         <nav className="sidebar-navigation">
@@ -24,10 +47,17 @@ export default function NavBar() {
                         className={index === activeIndex ? 'active' : ''}
                         onClick={() => setActiveIndex(index)}
                     >
-                        <Link to={item.to}>
-                            <FontAwesomeIcon icon={item.icon} />
-                            <span className="tooltip">{item.tooltip}</span>
-                        </Link>
+                        {item.onClick ? (
+                            <Link onClick={item.onClick}>
+                                <FontAwesomeIcon icon={item.icon} />
+                                <span className="tooltip">{item.tooltip}</span>
+                            </Link>
+                        ) : (
+                            <Link to={item.to}>
+                                <FontAwesomeIcon icon={item.icon} />
+                                <span className="tooltip">{item.tooltip}</span>
+                            </Link>
+                        )}
                     </li>
                 ))}
             </ul>
