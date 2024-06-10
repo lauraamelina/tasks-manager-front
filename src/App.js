8822bcd53fa5260328ad861b52c7c906d5f40fa9
lauraamelina <lauraamelina@gmail.com>
2024-06-10 15:17:09 -0300

    feat: add routes PageAddTask & PageListTask to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
+import PageAddTask from './pages/task/PageAddTask';
 import PageLogin from './pages/auth/PageLogin';
 import PageRegister from './pages/auth/PageRegister';
 import NavBar from './components/navbar/NavBar';
@@ -7,6 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/styles.css';
 import * as authService from './services/auth/auth.service';
 import { useEffect, useState } from 'react';
+import PageListTask from './pages/task/PageListTask';
 
 function App() {
   const [authenticated, setAuthenticated] = useState(false);
@@ -41,6 +43,8 @@ function App() {
       <NavBar authenticated={authenticated} setAuthenticated={setAuthenticated} />
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/add' element={<PageAddTask />} />
+        <Route path='/list' element={<PageListTask />} />
       </Routes>
     </>
   );
