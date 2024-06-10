921285cc8a3e656f9e2f184a8d396958c4efe372
lauraamelina <lauraamelina@gmail.com>
2024-06-10 10:31:46 -0300

    add HomePage component

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CardHome from '../../components/home/CardHome'
+
+export default function HomePage() {
+    return (
+        <main>
+            <h1>Task Manager</h1>
+            <CardHome />
+        </main>
+    )
+}
