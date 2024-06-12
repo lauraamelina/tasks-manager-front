32f7455c7c80ffbeb6306796ecd914d697686d3f
lauraamelina <lauraamelina@gmail.com>
2024-06-12 18:42:07 -0300

    feat: add function handleChangeStatus & loadingTaskId

--- src/components/list/TodoList.jsx
+++ src/components/list/TodoList.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPencilAlt, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-import { changeTaskStatus } from '../../services/tasks/task.service';
+import { CircularProgress } from '@mui/material';
 
-export default function TodoList({ tasks, states }) {
-    const handleStatusChange = async (taskId, newStatus) => {
-        try {
-            await changeTaskStatus(taskId, newStatus);
-            // Handle success (e.g., show a notification, update state)
-        } catch (error) {
-            console.error('Error changing task status:', error);
-            // Handle error (e.g., show an error notification)
-        }
-    };
+export default function TodoList({ tasks, states, handleStatusChange, loadingTaskId }) {
 
     const formatDate = (dateString) => {
         const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
@@ -24,16 +15,16 @@ export default function TodoList({ tasks, states }) {
             {tasks?.map(task => (
                 <div className="card mb-3" key={task.task_id}>
                     <div className="card-body row">
-                        <div className='col-md-8'>
+                        <div className='col-md-9'>
                             <h3 className="card-title fw-bold text-truncate" title={task.task_name}>
                                 {task.task_name}
                             </h3>
                             <p className="card-text text-muted text-truncate" title={task.description}>
                                 {task.description}
                             </p>
-                            <span className='card-date'>{formatDate(task.due_date)}</span>
+                            <span className='card-date'>Vencimiento: <span>{formatDate(task.due_date)}</span></span>
                         </div>
-                        <div className="col-md-4">
+                        <div className="col-md-3">
                             <div>
                                 <a href="#!" className="text-primary me-4 mt-auto" title="Edit todo">
                                     <FontAwesomeIcon icon={faPencilAlt} className="fa-lg" />
@@ -42,19 +33,24 @@ export default function TodoList({ tasks, states }) {
                                     <FontAwesomeIcon icon={faTrashAlt} className="fa-lg" />
                                 </a>
                             </div>
-                            <select
-                                className="form-select"
-                                value={task.status_id}
-                                onChange={(e) => handleStatusChange(task.task_id, e.target.value)}
-                            >
-                                {states.map(state => (
-                                    <option key={state.status_id} value={state.status_id}>
-                                        {state.name}
-                                    </option>
-                                ))}
-                            </select>
-
-
+                            <div className="d-flex align-items-center w-100 justify-content-center">
+                                {loadingTaskId === task.task_id ? (
+                                    <CircularProgress size={24} className="ms-auto" />
+                                ) : (
+                                    <select
+                                        className="form-select "
+                                        value={task.status_id}
+                                        onChange={(e) => handleStatusChange(task.task_id, e.target.value)}
+                                        disabled={loadingTaskId !== null}
+                                    >
+                                        {states.map(state => (
+                                            <option key={state.status_id} value={state.status_id}>
+                                                {state.name}
+                                            </option>
+                                        ))}
+                                    </select>
+                                )}
+                            </div>
                         </div>
                     </div>
                 </div>
