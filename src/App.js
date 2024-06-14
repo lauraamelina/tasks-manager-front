623ce00171d10bc6bae6bc53684ae3e62ee60389
lauraamelina <lauraamelina@gmail.com>
2024-06-14 10:27:37 -0300

    feat: add function isTokenExpired to App.js

--- src/App.js
+++ src/App.js
@@ -10,17 +10,26 @@ import * as authService from './services/auth/auth.service';
 import { useEffect, useState } from 'react';
 import PageListTask from './pages/task/PageListTask';
 import ErrorBoundary from './utils/ErrorBoundary';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 function App() {
   const [authenticated, setAuthenticated] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const user = authService.getUser();
-    if (user) {
+    if (user && !authService.isTokenExpired()) {
       setAuthenticated(true);
+    } else {
+      authService.logout();
+      toast.error('Your session has expired. Please log in again.', {
+        position: 'bottom-right',
+        autoClose: 5000,
+        onClose: () => navigate('/login')
+      });
     }
-  }, []);
-
+  }, [navigate]);
   function onLogin(result) {
     authService.setToken(result.token);
     authService.setUser(result.user);
