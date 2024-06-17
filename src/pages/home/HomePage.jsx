9172895f32ebfc9ccd03dd933f040c73c6edaa76
lauraamelina <lauraamelina@gmail.com>
2024-06-17 17:01:19 -0300

    feat: add services in HomePage

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getAllTasksByUser } from '../../services/tasks/task.service'
+import { getAllStates } from '../../services/states/state.service'
 import CardHome from '../../components/home/CardHome'
+import { getUser } from '../../services/auth/auth.service'
+import { toast } from 'react-toastify'
 
 export default function HomePage() {
+    const [user] = useState(getUser())
+    const [tasks, setTasks] = useState([])
+    const [states, setStates] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        getTask()
+        //eslint-disable-next-line
+    }, [user])
+
+    const getTask = async () => {
+        try {
+            setLoading(true);
+            const [statesResponse, tasksResponse] = await Promise.all([
+                getAllStates(),
+                getAllTasksByUser(user?.id)
+            ]);
+            setStates(statesResponse.data);
+            setTasks(tasksResponse.data);
+        } catch (error) {
+            toast.error('Error al cargar el dashboard', {
+                position: 'bottom-right',
+                autoClose: 5000,
+            });
+        } finally {
+            setLoading(false);
+        }
+
+
+    }
+
+    useEffect(() => {
+        console.log(tasks)
+    }, [tasks])
+
     return (
         <main>
             <h1>Task Manager</h1>
