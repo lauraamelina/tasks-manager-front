222bc9ee25034d30feefdb7b6bcc9867f3b33faf
lauraamelina <lauraamelina@gmail.com>
2024-06-17 15:24:03 -0300

    feat: add PageProfile

---
+++ src/pages/profile/PageProfile.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PageProfile() {
+    return (
+        <main>
+            <h1>Task Manager</h1>
+            <p>Bienvenido perfil</p>
+        </main>
+    )
+}
