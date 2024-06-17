cab52aa9a4083c1a0a24677bd3cbfe46cd74d015
lauraamelina <lauraamelina@gmail.com>
2024-06-17 15:31:15 -0300

    feat: add inputs name & email in PageProfile.jsx

--- src/pages/profile/PageProfile.jsx
+++ src/pages/profile/PageProfile.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { getUser } from '../../services/auth/auth.service'
 
 export default function PageProfile() {
+    const [user] = useState(getUser())
     return (
         <main>
             <h1>Task Manager</h1>
-            <p>Bienvenido perfil</p>
+            <div className="card profile">
+                <form action="">
+                    <div className="form-floating mb-3">
+                        <input type="name" className="form-control" id="floatingName" defaultValue={user?.name} />
+                        <label htmlFor="floatingName">Nombre</label>
+                    </div>
+                    <div className="form-floating mb-3">
+                        <input type="email" className="form-control" id="floatingemail" defaultValue={user?.email} />
+                        <label htmlFor="floatingemail">Email</label>
+                    </div>
+                </form>
+
+            </div>
         </main>
     )
 }
